8dc722dec7d61e29792a1d56bc70fa2615356dcc
jaredarmstrong58 <jaredarmstrong58@gmail.com>
2025-03-18 23:38:49 +0000

    deploy: edits for GitHub actions to use dist directory for deployment, up to date build and astro.config.mjs file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import dotenv from "dotenv";
-import node from '@astrojs/node';
-
-dotenv.config();
-
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
 
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [react()],
-
-  site: 'https://www.glowsalonwestclay.com',
-
-  output: 'static',
-
-  adapter: node({
-    mode: 'standalone',
-  })
-
+  site: "https://www.glowsalonwestclay.com/",
+  base: "/",
+  output: "static",
+  integrations: [sitemap(), tailwindcss()],
 });
