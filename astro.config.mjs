1a71637ddec595453aed3e3df095b0be6b70f9cc
jaredarmstrong58 <jaredarmstrong58@gmail.com>
2025-03-30 15:17:28 +0000

    fix: properly input the gtag configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: "https://www.glowsalonwestclay.com/",
   base: "/",
   output: "static",
-  integrations: [sitemap(), tailwindcss()],
+  integrations: [
+    sitemap(), 
+    tailwindcss(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      }
+    })
+  ],
 });
