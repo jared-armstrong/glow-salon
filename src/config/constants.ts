1f086458e1cd3db67a6070f8a137224bb6573356
Jared Armstrong <jaredarmstrong314@yahoo.com>
2026-01-16 14:18:28 -0500

    upgraded astro, added consultation to website

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -39,6 +39,7 @@ export const BUSINESS_INFO = {
     instagram: "https://www.instagram.com/glowsalonwestclay/",
   },
   bookingUrl: "https://glowsalonwestclay.glossgenius.com/services",
+  bridalConsultationUrl: "https://calendly.com/glowsalonwestclay/30min",
   formspreeId: "mkgjnwqo",
   aggregateRating: {
     ratingValue: "4.5",
